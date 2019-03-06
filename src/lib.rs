5ca3e835911b03332db3c86f2a5d5fe333feb1bf
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2019-03-06 21:10:00 +0900

    20.2.2.7 チャンネル経由でスレッドにリクエストを送信する

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 // スレッドプール構造体
 pub struct ThreadPool {
     workers: Vec<Worker>, // スレッドを直接保持するのではなく、Workerインスタンスを保持する
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// 新しいThreadPoolを生成する。
@@ -24,14 +30,24 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        // チャンネルを生成
+        let (sender, receiver) = mpsc::channel();
+
+        // 全ワーカー間で単独のreceiverを共有し、チャンネルキューから仕事を取りだす（receiverを可変化する）
+        //  複数のスレッドで所有権を共有しつつ、スレッドに値を可変化させる -> スレッド安全なスマートポインタにさせる
+        //    Arc: 複数の参照間で値の所有権を同時に共有できるようにする型
+        //    Mutex: 同時に一つのスレッドのなかでしか値は変更できないということを保証できる型
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size); // sizeキャパシティで初期化
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver))); // チャンネルの受信側をワーカーに渡す
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender, // チャンネルの送信側をThreadPoolインスタンスに保持する
         }
     }
 
@@ -53,9 +69,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
-
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
         // idとスレッドを保持するWorkerインスタンスを返す
         Worker {
             id,
