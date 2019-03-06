5a896d81c1e9436003c7d83ae4ea05d542ec1a32
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2019-03-06 21:09:42 +0900

    20.2.2.4 newでスレッド数を検査する

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,24 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
-    // size: スレッド数
+    /// 新しいThreadPoolを生成する。
+    ///
+    /// sizeがプールのスレッド数です。
+    ///
+    /// # パニック
+    ///
+    /// sizeが0なら、`new`関数はパニックします。
+    ///
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
     }
 
